ce1f65906542d9e11ce3a28fdca5c9ab49b14c7d
Orkun Akile <orkun.akile@gmail.com>
2024-04-22 22:45:21 +0200

    README update and CombatLogEntryRepository Fix

--- README.md
+++ README.md
@@ -17,3 +17,34 @@ hero name is missing, 404 Http code should be returned.
 - `CombatLogParser` consists of `CombatLogLineParser`s. There are currently 4 types of line parsers: Hero Killed, Item
 Purchase, Damage Done and Spell Cast parsers. When a new type of line parser needs to be implemented, it can be easily
 implemented by extending `CombatLogLineParser` class.
+
+## Technologies Used
+
+- Java 17
+- Spring Boot 3.2.5
+- Spring MVC
+- JUnit 5
+
+## Setup
+
+To run application locally, make sure you have Java and Maven installed on your machine. Clone this repository and run the Spring Boot application.
+
+```bash
+git clone https://github.com/deepend0/bayes-java-code-challenge
+cd bayes-java-code-challenge
+mvn spring-boot:run
+```
+
+## Testing
+
+Unit tests and integration tests are provided for both controllers. These tests ensure the correctness of controller behavior and error handling.
+
+To run the tests, execute the following command:
+
+```bash
+mvn test
+```
+
+## Contributors
+
+    Orkun Akile - orkun.akile@gmail.com
--- src/main/java/gg/bayes/challenge/persistence/repository/CombatLogEntryRepository.java
+++ src/main/java/gg/bayes/challenge/persistence/repository/CombatLogEntryRepository.java
@@ -13,15 +13,15 @@ import java.util.List;
 
 @Repository
 public interface CombatLogEntryRepository extends JpaRepository<CombatLogEntryEntity, Long> {
-    @Query("SELECT new gg.bayes.challenge.rest.model.HeroKills(cle.actor, CAST(count(cle.actor) AS Integer)) FROM CombatLogEntryEntity cle JOIN cle.match m WHERE m.id=:matchId AND cle.type = 'HERO_KILLED' GROUP BY cle.actor")
+    @Query("SELECT new gg.bayes.challenge.service.model.HeroKills(cle.actor, CAST(count(cle.actor) AS Integer)) FROM CombatLogEntryEntity cle JOIN cle.match m WHERE m.id=:matchId AND cle.type = 'HERO_KILLED' GROUP BY cle.actor")
     List<HeroKills> getMatchHeroKills(Long matchId);
 
-    @Query("SELECT new gg.bayes.challenge.rest.model.HeroItem(cle.item, cle.timestamp) FROM CombatLogEntryEntity cle JOIN cle.match m WHERE m.id=:matchId AND cle.type = 'ITEM_PURCHASED' AND cle.actor=:heroName")
+    @Query("SELECT new gg.bayes.challenge.service.model.HeroItem(cle.item, cle.timestamp) FROM CombatLogEntryEntity cle JOIN cle.match m WHERE m.id=:matchId AND cle.type = 'ITEM_PURCHASED' AND cle.actor=:heroName")
     List<HeroItem> getMatchHeroItems(Long matchId, String heroName);
 
-    @Query("SELECT new gg.bayes.challenge.rest.model.HeroSpells(cle.ability, CAST(count(cle.ability) AS Integer)) FROM CombatLogEntryEntity cle JOIN cle.match m WHERE m.id=:matchId AND cle.type = 'SPELL_CAST' AND cle.actor=:heroName GROUP BY cle.ability")
+    @Query("SELECT new gg.bayes.challenge.service.model.HeroSpells(cle.ability, CAST(count(cle.ability) AS Integer)) FROM CombatLogEntryEntity cle JOIN cle.match m WHERE m.id=:matchId AND cle.type = 'SPELL_CAST' AND cle.actor=:heroName GROUP BY cle.ability")
     List<HeroSpells> getMatchHeroSpells(Long matchId, String heroName);
 
-    @Query("SELECT new gg.bayes.challenge.rest.model.HeroDamage(cle.target, CAST(count(cle.target) AS Integer), CAST(sum(cle.damage) AS Integer)) FROM CombatLogEntryEntity cle JOIN cle.match m WHERE m.id=:matchId AND cle.type = 'DAMAGE_DONE' AND cle.actor=:heroName GROUP BY cle.target")
+    @Query("SELECT new gg.bayes.challenge.service.model.HeroDamage(cle.target, CAST(count(cle.target) AS Integer), CAST(sum(cle.damage) AS Integer)) FROM CombatLogEntryEntity cle JOIN cle.match m WHERE m.id=:matchId AND cle.type = 'DAMAGE_DONE' AND cle.actor=:heroName GROUP BY cle.target")
     List<HeroDamage> getMatchHeroDamage(Long matchId, String heroName);
 }
